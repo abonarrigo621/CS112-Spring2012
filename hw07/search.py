5f4f09f97152fbf19c60eb0fd0f0b33a5d72fbe3
Amber Bonarrigo <acb11@hampshire.edu>
2012-02-18 13:43:56 -0500

    hw07 done

--- hw07/search.py
+++ hw07/search.py
@@ -8,21 +8,32 @@ I used the Iterative implementation from here:
 """
 from hwtools import input_nums
 
-nums=inputNums()
-sorted(nums)
+#creates and sorts a list from least to greatest of numbers inputted by user
+nums = input_nums()
+N = sorted(nums)
 print "I have sorted your numbers"
-x=raw_input("Which number should I find: ")
-m=1
-M=len(nums)-1
-while M>=m:
-    md=M+m/2
-    if nums[md]==x:
-        break
-    elif x>nums[md]:
-       m=md+1
-     else:
-        M-=1
-if nums[md]=x:
-    print "Found", x "at", md
-else
-    print "Could not find", x
+print N
+
+x = int(raw_input("Which number should I find: "))
+min = int(1)
+max = N[-1]
+
+#Program finds x by reconstraining range until the midpoint equals x
+while max >= min:
+	mid = int((max + min) / 2)
+	print "midpoint =",mid
+	if x > mid:
+		min = mid + 1
+		print "Low"
+		print "new min =",min
+	elif x < mid:
+		max = mid - 1
+		print "High"
+		print "new max =",max
+	elif x == mid:
+		print "Found",x,"at min =",min,"max=",max
+		break
+	if min >= max:	
+		print "Could not find",x
+		break
+
